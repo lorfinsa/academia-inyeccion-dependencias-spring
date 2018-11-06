b2a70b54ebcf5a0592ffe268b9b6309e2ae3d881
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2018-11-06 10:34:43 -0300

    + comentario

--- InyeccionDemo/src/main/java/com/example/repository/PersonaJpaRepository.java
+++ InyeccionDemo/src/main/java/com/example/repository/PersonaJpaRepository.java
@@ -3,10 +3,13 @@ package com.example.repository;
 import com.example.domain.Persona;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+// En este caso la anotación es opcional. Spring ya detecta que la clase extiende de un JPARepository.
 public interface PersonaJpaRepository extends JpaRepository<Persona, Long> {
 
     //query method para buscar personas por nombre, ordenadas por descripcion de rol
     List<Persona> findByNombreIgnoreCaseOrderByRolDescripcionDesc(String unNombre);
-    
+
 }
