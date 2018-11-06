c5de4bc85ee5cf0f186c4e1cb7acfedc8f55e6ce
BUZZETTI1328 <BUZZETTI1328@NB-GESTION.clearpath.com>
2018-11-06 11:33:10 -0300

    Agrego vista dinamica del lado del servidor con thymeleaf

--- InyeccionDemo/src/main/java/com/example/controller/HomeController.java
+++ InyeccionDemo/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,29 @@
 package com.example.controller;
 
+import com.example.domain.Persona;
+import com.example.service.PersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    
+    @Autowired
+    PersonaService personaService;
 
     @GetMapping("/home")
     public String getHome(){
         return "home";
+    }
+
+    @GetMapping("/personas")
+    public String vistaPersonas(Model modelo){
+        List<Persona> personas = personaService.obtenerTodasLasPersonas();
+        modelo.addAttribute("listaPersonas", personas);
+        
+        return "personas";
     }
 }
